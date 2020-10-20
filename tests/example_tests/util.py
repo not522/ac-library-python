7c0fd44171797ef763635ad93eab0ea5ccbc6085
Naoto Mizuno <gobou522@gmail.com>
2020-10-20 09:07:14 +0900

    Test combined examples

--- tests/example_tests/util.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from subprocess import check_output
-import sys
-
-
-def run(source, stdin):
-    source_path = Path(__file__).parent.parent.parent / 'example' / source
-    return check_output((sys.executable, source_path), stdin=stdin)
